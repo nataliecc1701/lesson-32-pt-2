375f56ab424bd8b1ec33d3247726fdd1ddee2bea
Natalie Chapman <natalie.c.chapman628@gmail.com>
2024-05-30 18:14:12 -0700

    moved error handlers back into router

--- server.js
+++ server.js
@@ -6,28 +6,6 @@ const app = express()
 
 app.use("/items", router)
 
-/**
- * 
- * ERROR HANDLERS
- * 
-*/
-
-// 404 handler
-app.use(function(req, res, next) {
-    const notFoundError = new ExpressError("Not found", 404);
-    return next(notFoundError)
-})
-
-// general purpose error handler
-app.use(function(err, req, res, next) {
-    const status = err.status || 500
-    const message = err.message
-    
-    res.status(status).json({
-        error : {message, status}
-    })
-})
-
 
 /**
  * 
