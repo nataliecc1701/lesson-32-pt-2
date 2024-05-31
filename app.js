0057a2ca3e9bd9abdac5fb7e1a8ca917d3f78e9b
Natalie Chapman <natalie.c.chapman628@gmail.com>
2024-05-30 17:54:59 -0700

    patch route added

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
 const express = require("express")
 const items = require("./fakeDb")
+const ExpressError = require("./error")
 
 app = express()
 
 app.use(express.json())
+
+/**
+ * 
+ * ROUTES
+ * 
+ */
 
 app.get("/items", function(request,response) {
     return response.json(items)
@@ -14,13 +21,58 @@ app.post("/items", function(request,response) {
         const { name, price } = request.body
         const newItem = { name, price } // extract these and only these from request.body
         items.push(newItem)
-        return response.json({added : newItem})
+        return response.status(201).json({added : newItem})
     }
     else {
-        return response.status(400).json({message : "item needs a name and a price"})
+        return next(new ExpressError("Item needs a name and a price", 400))
     }
 })
 
+app.get("/items/:name", function(request, response) {
+    const item = items.find(item => item.name === request.params.name)
+    if (!item) throw new ExpressError("Not found", 404)
+        
+    return response.json(item)
+})
+
+app.patch("/items/:name", function(request, response) {
+    const item = items.find(item => item.name === request.params.name)
+    if (!item) {throw new ExpressError("Not found", 404)}
+    
+    
+    if (request.body.name) {item.name = request.body.name}
+    if (request.body.price) {item.price = request.body.price}
+    return response.json({updated : item})
+})
+
+/**
+ * 
+ * ERROR HANDLERS
+ * 
+*/
+
+// 404 handler
+app.use(function(req, res, next) {
+    const notFoundError = new ExpressError("Not found", 404);
+    return next(notFoundError)
+})
+
+// general purpose error handler
+app.use(function(err, req, res, next) {
+    const status = err.status || 500
+    const message = err.message
+    
+    res.status(status).json({
+        error : {message, status}
+    })
+})
+
+
+/**
+ * 
+ * LISTENER
+ * 
+ */
 
 const port = 3000;
 app.listen(port, function() {
